d70f4818c3df1cb7b83837d3956b36f970fd7fef
Trishlaa07 <trishlaa.s07@gmail.com>
2026-02-11 17:55:24 +0530

    Backend setup with JWT authentication and protected routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const protect = require("../middleware/authMiddleware");
+
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+    role
+  });
+
+  res.status(201).json({
+    message: "User registered successfully",
+    user: { id: user._id, name: user.name, email: user.email, role: user.role }
+  });
+});
+
+// LOGIN 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: "Invalid email or password" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "Invalid email or password" });
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+
+  res.json({
+    message: "Login successful",
+    token,
+    user: { id: user._id, name: user.name, email: user.email, role: user.role }
+  });
+});
+
+// GET USER PROFILE 
+router.get("/profile", protect, async (req, res) => {
+  const user = await User.findById(req.user.id).select("-password");
+  res.json(user);
+});
+
+
+module.exports = router;
